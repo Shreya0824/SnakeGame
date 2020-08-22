9c316734eb360bb5f72bda048c9c192a4aa8f4f5
Shreya Goel <goel8002@gmail.com>
2020-08-22 13:48:15 +0530

    first commit

---
+++ MachineLearning/MultipleLinearRegression/HouseRent.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn import  preprocessing,linear_model
+import numpy as np
+import sklearn
+
+####### LOAD DATA #####
+data =pd.read_csv('houses_to_rent.csv', sep=',')
+data=data[['city','rooms','bathroom','parking spaces', 'fire insurance','furniture','rent amount']]
+print(data.head())
+
+##### PROCESS DATA #####
+data['rent amount'] = data['rent amount'].map(lambda i: int( i[2:].replace(',','')))
+data['fire insurance'] = data['fire insurance'].map(lambda i: int( i[2:].replace(',','')))
+le = preprocessing.LabelEncoder()
+data['furniture'] = le.fit_transform(data['furniture'] )
+print(data.head())
+
+##### SPLIT DATA ######
+x=np.array(data.drop(['rent amount'],1))
+y=np.array(data['rent amount'])
+print('X',x.shape)
+print('Y',y.shape)
+
+xTrain , xTest , yTrain , yTest = sklearn.model_selection.train_test_split(x,y,test_size=0.2,random_state=20)
+
+print('X_TRAIN', xTrain.shape)
+print('X_TEST', xTest.shape)
+
+
+###### TRAINING  ######
+model =linear_model.LinearRegression()
+model.fit(xTrain,yTrain)
+accuracy =model.score(xTest,yTest)
+print('Coeffecients:', model.coef_)
+print('Intercept:', model.intercept_)
+print('Acuuracy:', round(accuracy*100,3),'%')
+
+#### EVALUATION ####
+testVals =model.predict(xTest)
+error =[]
+for i,testVal in enumerate(testVals):
+    error.append(yTest[i]-testVal)
+    print(f'Actual:{yTest[i]} Predicyion:{int(testVal)} Error: {int(error[i])}')
